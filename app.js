0d6602bfb6dc6233b6292805982e717f48af09c3
Mzeeshanriaz <36808956+Mzeeshanriaz@users.noreply.github.com>
2018-07-12 18:11:29 +0500

    api data accessed now

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ angular.module("App",["ngStorage"]).controller("mainController",function($scope,
     var data = "Zeeshan";
     $scope.data = data;
     $scope.clients = [];
-    this.databtn =true;
+    $scope.databtn =true;
+    if(!$localStorage.key) $scope.databtn = true;
     $scope.login = function () {
         if(!$localStorage.key)
         $http.post("http://localhost:8000/api/v1/auth",
             {email : 'Arch.Streich@Roberts.com',password: 'secret'} ).then(function (res) {
             $localStorage.key = res.data.token;
         });
-        this.databtn=true;
+        $scope.databtn=true;
     };
     $scope.getClients = function () {
+        var Url = "http://localhost:8000/api/v1/clients?limit=25&page=1&searchFilters%5BsearchText%5D=&searchFilters%5Bstatus%5D%5Binactive%5D%5Bstate%5D=false&searchFilters%5Bstatus%5D%5Bactive%5D%5Bstate%5D=true&searchFilters%5Bfrequency%5D%5B0%5D%5Babbrev%5D=weekly&searchFilters%5Bfrequency%5D%5B0%5D%5Bvalue%5D=1&searchFilters%5Bfrequency%5D%5B0%5D%5Bstate%5D=true&searchFilters%5Bfrequency%5D%5B0%5D%5B%24%24hashKey%5D=object%3A104&searchFilters%5Bfrequency%5D%5B1%5D%5Babbrev%5D=bi-weekly&searchFilters%5Bfrequency%5D%5B1%5D%5Bvalue%5D=2&searchFilters%5Bfrequency%5D%5B1%5D%5Bstate%5D=true&searchFilters%5Bfrequency%5D%5B1%5D%5B%24%24hashKey%5D=object%3A105&searchFilters%5Bfrequency%5D%5B2%5D%5Babbrev%5D=monthly&searchFilters%5Bfrequency%5D%5B2%5D%5Bvalue%5D=4&searchFilters%5Bfrequency%5D%5B2%5D%5Bstate%5D=true&searchFilters%5Bfrequency%5D%5B2%5D%5B%24%24hashKey%5D=object%3A106&searchFilters%5Bfrequency%5D%5B3%5D%5Babbrev%5D=one+time&searchFilters%5Bfrequency%5D%5B3%5D%5Bvalue%5D=0&searchFilters%5Bfrequency%5D%5B3%5D%5Bstate%5D=true&searchFilters%5Bfrequency%5D%5B3%5D%5B%24%24hashKey%5D=object%3A107";
         if($localStorage.key) {
             var config = {
                 headers: {
@@ -19,8 +21,8 @@ angular.module("App",["ngStorage"]).controller("mainController",function($scope,
                     'Authorization': 'Bearer ' + $localStorage.key
                 }
             };
-            $http.get("http://localhost:8000/api/v1/clients?limit=25&page=1", config).then(function (res) {
-                $scope.clients = res.data;
+            $http.get(Url, config).then(function (res) {
+                $scope.clients = res.data.data.data;
             });
         }
     }
